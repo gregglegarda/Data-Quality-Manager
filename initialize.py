b60b58e709e8f84eca9cab5a78a71ab3273d3805
gregglegarda <gregglegarda@gwu.edu>
2022-08-15 22:21:08 -0400

    commit2

---
+++ initialize.py
@@ -0,0 +1,41 @@
+import plotly.graph_objs as go
+########### Set up the chart
+
+
+########### Define your variables
+tabtitle='beer tab title!'
+myheading='Data Quality Manager'
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='darkred'
+color2='orange'
+mytitle='Directory Name'
+label1='IBU'
+label2='ABV'
+githublink='https://github.com/austinlasseter/flying-dog-beers'
+sourceurl='https://www.flyingdog.com/beers/'
+
+
+def setup_chart():
+    bitterness = go.Bar(
+        x=beers,
+        y=ibu_values,
+        name=label1,
+        marker={'color':color1}
+    )
+    alcohol = go.Bar(
+        x=beers,
+        y=abv_values,
+        name=label2,
+        marker={'color':color2}
+    )
+
+    beer_data = [bitterness, alcohol]
+    beer_layout = go.Layout(
+        barmode='group',
+        title = mytitle
+    )
+
+    beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+    return beer_fig
